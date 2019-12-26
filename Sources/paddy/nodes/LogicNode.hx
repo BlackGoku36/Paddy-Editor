34ada3c147c52212008465330ad6ee4ded3eaaaa
BlackGoku36 <urjasvisuthar@gmail.com>
2019-12-26 16:46:06 +0530

    Format

--- Sources/paddy/nodes/LogicNode.hx
+++ Sources/paddy/nodes/LogicNode.hx
@@ -5,69 +5,69 @@ import zui.Nodes.TNode;
 
 class LogicNode {
 
-    public static var gate: TNode = {
-        id: 0,
-        name: "Gate",
-        type: "GateNode",
-        x: 200,
-        y: 200,
-        color: -4962746,
-        inputs: [
-            {
-                id: 0,
-                node_id: 0,
-                name: "In",
-                type: "ACTION",
-                color: Color.fromBytes(90, 220, 90),
-                default_value: 0.0
-            },
-            {
-                id: 1,
-                node_id: 0,
-                name: "Bool",
-                type: "BOOLEAN",
-                color: Color.fromBytes(90, 220, 90),
-                default_value: 0.0
-            },
-            {
-                id: 2,
-                node_id: 0,
-                name: "Bool",
-                type: "BOOLEAN",
-                color: Color.fromBytes(90, 220, 90),
-                default_value: 0.0
-            }
-        ],
-        outputs: [
-            {
-                id: 0,
-                node_id: 0,
-                name: "True",
-                type: "BOOL",
-                color: Color.fromBytes(90, 220, 90),
-                default_value: 0.0
-            },
-            {
-                id: 1,
-                node_id: 0,
-                name: "False",
-                type: "BOOL",
-                color: Color.fromBytes(90, 220, 90),
-                default_value: 0.0
-            }
-        ],
-        buttons: [
-            {
-                name: "operations",
-                type: "ENUM",
-                data: [
-                    "OR",
-                    "AND",
-                    "Equal",
-                    "Less", "Less Equal",
-                    "Greater", "Greater Equal"
-                ]
-            }
-        ]
-    }
+	public static var gate: TNode = {
+		id: 0,
+		name: "Gate",
+		type: "GateNode",
+		x: 200,
+		y: 200,
+		color: -4962746,
+		inputs: [
+			{
+				id: 0,
+				node_id: 0,
+				name: "In",
+				type: "ACTION",
+				color: Color.fromBytes(90, 220, 90),
+				default_value: 0.0
+			},
+			{
+				id: 1,
+				node_id: 0,
+				name: "Bool",
+				type: "BOOLEAN",
+				color: Color.fromBytes(90, 220, 90),
+				default_value: 0.0
+			},
+			{
+				id: 2,
+				node_id: 0,
+				name: "Bool",
+				type: "BOOLEAN",
+				color: Color.fromBytes(90, 220, 90),
+				default_value: 0.0
+			}
+		],
+		outputs: [
+			{
+				id: 0,
+				node_id: 0,
+				name: "True",
+				type: "BOOL",
+				color: Color.fromBytes(90, 220, 90),
+				default_value: 0.0
+			},
+			{
+				id: 1,
+				node_id: 0,
+				name: "False",
+				type: "BOOL",
+				color: Color.fromBytes(90, 220, 90),
+				default_value: 0.0
+			}
+		],
+		buttons: [
+			{
+				name: "operations",
+				type: "ENUM",
+				data: [
+					"OR",
+					"AND",
+					"Equal",
+					"Less", "Less Equal",
+					"Greater", "Greater Equal"
+				]
+			}
+		]
+	}
 }
--- Sources/paddy/nodes/StdNode.hx
+++ Sources/paddy/nodes/StdNode.hx
@@ -5,132 +5,132 @@ import zui.Nodes.TNode;
 
 class StdNode {
 
-    public static var parseInt: TNode = {
-        id: 0,
-        name: "Parse Int",
-        type: "ParseIntNode",
-        x: 200,
-        y: 200,
-        color: -4962746,
-        inputs: [
-            {
-                id: 0,
-                node_id: 0,
-                name: "String",
-                type: "STRING",
-                color: Color.fromBytes(180, 180, 180),
-                default_value: ""
-            }
-        ],
-        outputs: [
-            {
-                id: 0,
-                node_id: 0,
-                name: "Int",
-                type: "VALUE",
-                color: Color.fromBytes(180, 180, 180),
-                default_value: ""
-            }
-        ],
-        buttons: []
-    }
+	public static var parseInt: TNode = {
+		id: 0,
+		name: "Parse Int",
+		type: "ParseIntNode",
+		x: 200,
+		y: 200,
+		color: -4962746,
+		inputs: [
+			{
+				id: 0,
+				node_id: 0,
+				name: "String",
+				type: "STRING",
+				color: Color.fromBytes(180, 180, 180),
+				default_value: ""
+			}
+		],
+		outputs: [
+			{
+				id: 0,
+				node_id: 0,
+				name: "Int",
+				type: "VALUE",
+				color: Color.fromBytes(180, 180, 180),
+				default_value: ""
+			}
+		],
+		buttons: []
+	}
 
-    public static var parseFloat: TNode = {
-        id: 0,
-        name: "Parse Float",
-        type: "ParseFloatNode",
-        x: 200,
-        y: 200,
-        color: -4962746,
-        inputs: [
-            {
-                id: 0,
-                node_id: 0,
-                name: "String",
-                type: "STRING",
-                color: Color.fromBytes(180, 180, 180),
-                default_value: ""
-            }
-        ],
-        outputs: [
-            {
-                id: 0,
-                node_id: 0,
-                name: "Float",
-                type: "VALUE",
-                color: Color.fromBytes(180, 180, 180),
-                default_value: ""
-            }
-        ],
-        buttons: []
-    }
+	public static var parseFloat: TNode = {
+		id: 0,
+		name: "Parse Float",
+		type: "ParseFloatNode",
+		x: 200,
+		y: 200,
+		color: -4962746,
+		inputs: [
+			{
+				id: 0,
+				node_id: 0,
+				name: "String",
+				type: "STRING",
+				color: Color.fromBytes(180, 180, 180),
+				default_value: ""
+			}
+		],
+		outputs: [
+			{
+				id: 0,
+				node_id: 0,
+				name: "Float",
+				type: "VALUE",
+				color: Color.fromBytes(180, 180, 180),
+				default_value: ""
+			}
+		],
+		buttons: []
+	}
 
-    public static var floatToInt: TNode = {
-        id: 0,
-        name: "Float to Int",
-        type: "FloatToIntNode",
-        x: 200,
-        y: 200,
-        color: -4962746,
-        inputs: [
-            {
-                id: 0,
-                node_id: 0,
-                name: "Float",
-                type: "VALUE",
-                color: Color.fromBytes(180, 180, 180),
-                default_value: ""
-            }
-        ],
-        outputs: [
-            {
-                id: 0,
-                node_id: 0,
-                name: "Int",
-                type: "VALUE",
-                color: Color.fromBytes(180, 180, 180),
-                default_value: ""
-            }
-        ],
-        buttons: []
-    }
+	public static var floatToInt: TNode = {
+		id: 0,
+		name: "Float to Int",
+		type: "FloatToIntNode",
+		x: 200,
+		y: 200,
+		color: -4962746,
+		inputs: [
+			{
+				id: 0,
+				node_id: 0,
+				name: "Float",
+				type: "VALUE",
+				color: Color.fromBytes(180, 180, 180),
+				default_value: ""
+			}
+		],
+		outputs: [
+			{
+				id: 0,
+				node_id: 0,
+				name: "Int",
+				type: "VALUE",
+				color: Color.fromBytes(180, 180, 180),
+				default_value: ""
+			}
+		],
+		buttons: []
+	}
 
-    public static var print: TNode = {
-        id: 0,
-        name: "Print",
-        type: "PrintNode",
-        x: 200,
-        y: 200,
-        color: -4962746,
-        inputs: [
-            {
-                id: 0,
-                node_id: 0,
-                name: "In",
-                type: "ACTION",
-                color: 0xffaa4444,
-                default_value: ""
-            },
-            {
-                id: 1,
-                node_id: 0,
-                name: "Value",
-                type: "STRING",
-                color: 0xff44aa44,
-                default_value: ""
-            }
-        ],
-        outputs: [
-            {
-                id: 0,
-                node_id: 0,
-                name: "Out",
-                type: "ACTION",
-                color: 0xffaa4444,
-                default_value: ""
-            }
-        ],
-        buttons: []
-    }
+	public static var print: TNode = {
+		id: 0,
+		name: "Print",
+		type: "PrintNode",
+		x: 200,
+		y: 200,
+		color: -4962746,
+		inputs: [
+			{
+				id: 0,
+				node_id: 0,
+				name: "In",
+				type: "ACTION",
+				color: 0xffaa4444,
+				default_value: ""
+			},
+			{
+				id: 1,
+				node_id: 0,
+				name: "Value",
+				type: "STRING",
+				color: 0xff44aa44,
+				default_value: ""
+			}
+		],
+		outputs: [
+			{
+				id: 0,
+				node_id: 0,
+				name: "Out",
+				type: "ACTION",
+				color: 0xffaa4444,
+				default_value: ""
+			}
+		],
+		buttons: []
+	}
 
 }
