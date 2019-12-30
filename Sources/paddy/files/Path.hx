210f9b3b8e91ecb2f5e996a33ad4e4a23303996a
BlackGoku36 <urjasvisuthar@gmail.com>
2019-12-30 22:19:23 +0530

    Improve path

--- Sources/paddy/files/Path.hx
+++ Sources/paddy/files/Path.hx
@@ -2,8 +2,16 @@ package paddy.files;
 
 class Path {
     
-    public static function getNameFromPath(name:String):String {
-		var nameArr = name.split("/");
-		return nameArr[nameArr.length-1];
+    public static function getNameFromPath(name:String, ext:Bool=true):String {
+        var outName = "";
+        if(ext){
+            var nameArr = name.split("/");
+            outName = nameArr[nameArr.length-1];
+        }else{
+            var nameArr = name.split("/");
+            var extname = nameArr[nameArr.length-1];
+            outName = extname.split(".")[0];
+        }
+        return outName;
 	}
 }
